a47b4a195883fc93f63512a1db2b41201bb355e6
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-08-23 12:40:26 -0400

    build payload add keyword list

--- trends.py
+++ trends.py
@@ -8,3 +8,8 @@ import matplotlib.pyplot as plt
 import requests
 
 pytrends = TrendReq(hl='en-US', timeout=5) #connect to google
+
+#keywords list
+keyWords = ['Artificial Intelligence', 'Facial Recognition']
+#Timeframe is from today to the last 3 months
+pytrends.build_payload(kw_list=keyWords,timeframe='today 3-m', geo='US')
