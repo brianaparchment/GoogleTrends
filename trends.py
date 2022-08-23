0e94458da979c7af05de82e1f7375f74bfe68ad0
brianaparchment <34357764+brianaparchment@users.noreply.github.com>
2022-08-23 12:38:32 -0400

    imports

---
+++ trends.py
@@ -0,0 +1,8 @@
+#The program uses Pytrends to find trends for search terms
+#InterestByRegion output is saved to intr_byRegion.png
+#RelatedQueries output is saved to ai.csv and facial_rec.csv
+import pandas as pd
+from pytrends.request import TrendReq
+import matplotlib
+import matplotlib.pyplot as plt
+import requests
